1d0a61538be19d4864d0ba088d1628c385445dcc
Rodrigo Pequeno <rodrigo.pequeno6@gmail.com>
2024-01-02 02:17:19 -0300

    fix: readme and example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Icon(
-              RemixIcon.aB,
+              RemixIcon.flutterLine,
               size: 48,
               color: Colors.red,
             ),
